636a180685b36cf1d2bc991e82464e8c4e0e353c
Daoud Clarke <daoud.clarke@gmail.com>
2013-03-07 18:07:26 +0000

    Extract dimensions in evaluation

--- evaluate.py
+++ evaluate.py
@@ -21,8 +21,8 @@ def get_mean_and_error(datasets, function):
     data = [function(x) for x in datasets]
     return np.mean(data), np.std(data)/math.sqrt(len(data))
 
-def evaluate(experiment):
     #print experiment
+def collect_results(experiment):
     suite = PyExperimentSuite()
     params = suite.get_params(experiment)
     reps = params['repetitions']
@@ -32,10 +32,13 @@ def evaluate(experiment):
         if len(results) == 0:
             raise MissingDataException
         datasets.append(results)
+    return datasets
         # print results['info']
         # confusion = results['confusion'][0]
         # print confusion, accuracy(confusion)
 
+def evaluate(experiment):
+    datasets = collect_results(experiment)
     evaluations = [
         ('accuracy', lambda x: accuracy(x['confusion'][0])),
         ('time', lambda x: x['time'][0])]
@@ -47,6 +50,8 @@ def evaluate(experiment):
     summary = [(name, get_mean_and_error(datasets, eval_func))
                for name, eval_func in evaluations]
 
+    suite = PyExperimentSuite()
+    params = suite.get_params(experiment)
     row = [(x,params[x]) for x in ['dataset','classifier']]
     for name, (value, error) in summary:
         row.append( (name,value) )
@@ -55,9 +60,10 @@ def evaluate(experiment):
     return row
 
 
-def write_summary(rows):
+def write_summary(rows, name):
+    print rows
     fieldnames = [x[0] for x in rows[0]]
-    output = csv.DictWriter(open('output.csv','w'), fieldnames)
+    output = csv.DictWriter(open(name,'w'), fieldnames)
     output.writeheader()
     output.writerows([dict(x) for x in rows])
 
@@ -74,9 +80,40 @@ def evaluate_all(path):
         except MissingDataException:
             continue
 
-    write_summary(rows)
-        
+    write_summary(rows, 'output.csv')
+
+def evaluate_dimensions(path):
+    suite = PyExperimentSuite() 
+    rows = []
+    for experiment in os.listdir(path):
+        try:
+            joined = os.path.join(path,experiment)
+            if os.path.isdir(joined):
+                params = suite.get_params(joined)
+                if params['classifier'] != 'cone':
+                    continue
+                datasets = collect_results(joined)
+                dimensions = params['dimensions']
+                values = {d:[] for d in dimensions}
+                for data in datasets:
+                    for dimension_row in data['info'][0]:
+                        d = dimension_row[0]['dimensions']
+                        values[d] += dimension_row[2]
+                print values
+                means = [(d, get_mean_and_error(values[d],
+                                                lambda x:x))
+                         for d in dimensions]
+                rows = [[('dimensions', x[0]),
+                         ('accuracy', x[1][0]),
+                         ('error', x[1][1])] for x in means]
+                write_summary(rows, params['dataset'] + '_dims.csv')
+
+        except MissingDataException:
+            continue
+
+    
 
 if __name__ == "__main__":
     path = sys.argv[1]
     evaluate_all(path)
+    evaluate_dimensions(path)
