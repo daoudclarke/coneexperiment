7adf771292115c399d65fd8901e5038d48173cac
Daoud Clarke <daoud.clarke@gmail.com>
2013-04-11 08:19:02 +0100

    Refactor tidy.py to make it more robust

--- analysis/tidy.py
+++ analysis/tidy.py
@@ -5,7 +5,7 @@
 import csv
 import sys
 
-
+from itertools import groupby
 
 def short(f):
     return '%.3f' % float(f)
@@ -13,31 +13,18 @@ def short(f):
 def tidy(filename):
     f = open(file_name)
     reader = csv.DictReader(f)
-    data = [x for x in reader]
-    data.sort(key=lambda x: x['dataset'] + x['classifier'])
+    data = sorted(reader, key=lambda x: x['dataset'] + x['classifier'])
+    
     fieldnames = ['Dataset', 'SVM', 'Decision Tree', 'Cone']
+    field_map = {'tree':'Decision Tree', 'svm':'SVM', 'cone':'Cone'}
     output = csv.DictWriter(sys.stdout, fieldnames)
     output.writeheader()
-    for i in range(0, len(data), 5):
-        cone = data[i]
-        cone_greedy = data[i+1]
-        stratified = data[i+2]
-        svm = data[i+3]
-        tree = data[i+4]
-        assert cone['classifier'] == 'cone'
-        #assert cone_greedy['classifier'] == 'cone-greedy'
-        #assert stratified['classifier'] == 'stratified'
-        assert svm['classifier'] == 'svm'
-        assert tree['classifier'] == 'tree'
-        assert cone['dataset'] == svm['dataset']
-        assert svm['dataset'] == tree['dataset']
+    for k, g in groupby(data, key = lambda x: x['dataset']):
+        classifiers = {x['classifier']:x for x in g}
         pm = ' $\pm$ '
-        columns = {'Dataset': cone['dataset'],
-                   'Stratified': short(stratified['accuracy']) + pm + short(stratified['accuracy error']),
-                   'SVM': short(svm['accuracy']) + pm + short(svm['accuracy error']),
-                   'Decision Tree': short(tree['accuracy']) + pm + short(tree['accuracy error']),
-                   'Cone (Gradient)': short(cone['accuracy']) + pm + short(cone['accuracy error']),
-                   'Cone (Greedy)' : short(cone_greedy['accuracy']) + pm + short(cone_greedy['accuracy error'])}
+        columns = {'Dataset': k}
+        for c, row in classifiers.items():
+            columns[field_map[c]] = short(row['accuracy']) + pm + short(row['accuracy error'])
 
         output.writerow(columns)
 
