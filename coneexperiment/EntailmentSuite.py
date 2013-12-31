123c7a29f8811c5489a4193da030c0690f3012dc
julieweeds <julie.weeds@gmail.com>
2013-12-31 12:02:20 +0000

    renamed params to classparams in iterate

--- coneexperiment/EntailmentSuite.py
+++ coneexperiment/EntailmentSuite.py
@@ -72,7 +72,7 @@ class EntailmentSuite(PyExperimentSuite):
     def iterate(self, params, rep, n):
         logging.info("Beginning iteration %d, repetition %d", n, rep)
         assert n == 0
-        confusion, time, info, params, predictions, target = self.experiment.runFold(rep)
+        confusion, time, info, classparams, predictions, target = self.experiment.runFold(rep)
         
         return {'rep':rep,
                 'iter':n,
@@ -80,7 +80,7 @@ class EntailmentSuite(PyExperimentSuite):
                 'time':time.total_seconds(),
                 'classifier':params['classifier'],
                 'info': info,
-                'params': params,
+                'params': classparams,
                 'predictions': predictions,
                 'target': target}
 
